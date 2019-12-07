e0eed9f6c8add84e17ddbfcb0226a1d5384a6a92
Daniel Irvine <daniel@danielirvine.com>
2019-12-07 21:08:20 +0000

    Add success/failure messages, and a describe block

--- src/runner.js
+++ src/runner.js
@@ -1,20 +1,37 @@
 import path from 'path';
 import { color } from './colors.js';
 
+let successes = 0;
+let failures = 0;
+
+const exitCodes = {
+  ok: 0,
+  failures: 1
+};
+
 export const run = async () => {
   try {
-  await import(path.resolve(process.cwd(), "test/tests.js"));
+    await import(path.resolve(process.cwd(), "test/tests.js"));
   } catch(e) {
     console.error(e);
   }
-  console.log('Test run finished');
+  console.log(color(`<green>${successes}</green> tests passed, <red>${failures}</red> tests failed.`));
+  process.exit(failures !== 0 ? exitCodes.failures : exitCodes.ok);
 };
 
 export const it = (name, body) => {
   try {
     body();
+    console.log(color(`  <green>✓</green> ${name}`));
+    successes++;
   } catch (e) {
-    console.error(color(`<red>${name}</red>`));
+    console.error(color(`  <red>✗</red> ${name}`));
     console.error(e);
+    failures++;
   }
 };
+
+export const describe = (name, body) => {
+  console.log(name);
+  body();
+}
