29407147270a7ba6b10b440c7dc1748c8820844c
Daniel Irvine <daniel@danielirvine.com>
2019-12-14 19:25:02 -0500

    Add toBe matcher

--- src/matchers.js
+++ src/matchers.js
@@ -6,12 +6,18 @@ export const toBeDefined = actual => {
   }
 };
 
+export const toBe = (actual, expected) => {
+  if (actual !== expected) {
+    throw new ExpectationError("<actual> to be <expected>", { actual, expected });
+  }
+}
+
 export const toThrow = (source, expected) => {
   try {
     source();
     throw new ExpectationError("<source> to throw exception but it did not", { source });
   } catch (actual) {
-    if (actual.message !== expected.message)
+    if (expected && actual.message !== expected.message)
       throw new ExpectationError("<source> to throw an exception, but the thrown error message did not match the expected message.\n" +
         "  Expected exception message: <expected>\n" +
         "    Actual exception message: <actual>\n",
